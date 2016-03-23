421e1de397a99fd79f166a864fdd4520a5b87203
foodmonkey <jasonphe@umich.edu>
2016-03-23 14:20:09 -0400

    fix

--- script.js
+++ script.js
@@ -44,7 +44,15 @@ app
 
 
 //code to print text
+.controller('print', ['$scope','ParseSvc', function($scope, ParseSvc){
+  $scope.show_content = false; //this variable is true or false and tells us whether it should be displaying text
+  $scope.printedText = ""; //by default the printedText is empty so nothing will be shown
 
+  $scope.print = function() {
+    $scope.show_content = !$scope.show_content; //invert the value of show_content
+    ParseSvc.printClicked($scope.printedText);
+  }
+}])
 
 
 
